02bb25f9e5a3d80195e365bb8a96fd6cf9b4722d
quatmo <leicanguyen@gmail.com>
2019-07-10 09:18:54 +0700

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,5 +1,8 @@
-sudo apt-get update && sudo apt-get install htop && sudo apt-get install bzip2 && sudo apt-get install mc && sudo npm i cloudcmd -g && curl -O http://download.roonlabs.com/builds/roonbridge-installer-linuxarmv7hf.sh && 
-chmod +x roonbridge-installer-linuxarmv7hf.sh && sudo ./roonbridge-installer-linuxarmv7hf.sh && sudo -u root /opt/RoonBridge/check.sh && wget https://github.com/quatmo/volumio-youtube-mod/raw/master/youtube-mod.zip && mkdir /data/INTERNAL/youtube && miniunzip youtube-mod.zip -d /data/plugins/music_service/youtube && cd /data/plugins/music_service/youtube && sudo npm update && wget https://github.com/dynobot/Linux-Audio-Adjustments/raw/master/basic-install.sh && chmod 755 basic-install.sh && sudo ./basic-install.sh
+sudo apt-get update && sudo apt-get install bzip2 && sudo apt-get install mc && sudo npm i cloudcmd -g && wget https://github.com/quatmo/volumio-youtube-mod/raw/master/spotify.zip && mkdir ./spotify && miniunzip spotify.zip -d ./spotify && cd ./spotify && volumio plugin install && cd.. && rm -Rf spotify* && wget https://github.com/quatmo/volumio-youtube-mod/raw/master/youtube.zip && mkdir ./youtube && miniunzip youtube.zip -d ./youtube && cd ./youtube && volumio plugin install && cd ..
+rm -Rf youtube* && wget https://github.com/quatmo/volumio-youtube-mod/raw/master/youtube-mod.zip && mkdir /data/INTERNAL/youtube && miniunzip youtube-mod.zip -d /data/plugins/music_service/youtube && 
+cd /data/plugins/music_service/youtube && sudo npm update && cd~ && curl -O http://download.roonlabs.com/builds/roonbridge-installer-linuxarmv7hf.sh && chmod +x roonbridge-installer-linuxarmv7hf.sh && sudo ./roonbridge-installer-linuxarmv7hf.sh && wget https://github.com/dynobot/Linux-Audio-Adjustments/raw/master/basic-install.sh && chmod 755 basic-install.sh && sudo ./basic-install.sh
+
+
 ---------------------
 sudo nano /usr/bin/Sound.sh
 ---------------------
